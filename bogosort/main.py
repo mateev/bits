abaf577c9dfc97b2ce7042a790386147e1102594
Ivan Mateev <1103222@abertay.ac.uk>
2012-04-05 22:55:25 +0100

    Extended functionality

--- bogosort/main.py
+++ bogosort/main.py
@@ -1,7 +1,15 @@
 from random import shuffle
-
+from random import seed
 
 def bogosort(listInput):
-    'Sorts a list'
+    'Sorts a list; returns how many iterations did it take'
+    count = 0
+    seed()
     while listInput!=sorted(listInput):
         shuffle(listInput)
+        count += 1
+    return count
+
+def bogosortNoCount(listInput):
+    'Sorts a list'
+    bogosort(listInput)
